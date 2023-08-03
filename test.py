c66fd6a0f325a32cff10e5db99db24f8cd196a6b
Dooshantsing Kautick <20820301@student.curtin.edu.au>
2023-08-03 09:11:03 +0400

    New Commit

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+value="Hello World"
+list=[1,2,4,5,3,6]
+
+print(value[0])
+print(value[-1])
+print(value[2:5])
+print(value[:5])
+print(list[::-5])
+print(value.split('l',maxsplit=1))
+print(value.upper())
